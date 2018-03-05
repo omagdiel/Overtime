76312ee2dec019b73bb22498fe334b743d5c4940
omagdiel <omagdiel@gmail.com>
2018-03-05 08:07:41 -0700

    Implemented ablility to block users after post approved

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -1,17 +1,18 @@
-describe 'navigate' do
-  before do
-    @admin_user = FactoryGirl.create(:admin_user)
-    login_as(@admin_user, :scope => :user)
-  end
+require 'rails_helper'
 
-  describe 'edit' do
+describe 'navigate' do
+	before do
+		@admin_user = FactoryGirl.create(:admin_user)
+		login_as(@admin_user, :scope => :user)
+	end
+
+	describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
       visit edit_post_path(@post)
     end
 
     it 'has a status that can be edited on the form by an admin' do
-
       choose('post_status_approved')
       click_on "Save"
 
@@ -27,5 +28,17 @@ describe 'navigate' do
 
       expect(page).to_not have_content('Approved')
     end
-  end
+
+    it 'should not be editable by the post creator if status is approved' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to eq(root_path)
+    end
+	end
 end
